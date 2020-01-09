1fb8a20c8dc32dc92692200680d3a35f5b6edd83
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-09 14:39:08 +0100

    Created service for storing and retrieving node uptime

---
+++ src/Services/NodeUptimeService.ts
@@ -0,0 +1,15 @@
+import {NodeUptime} from "../Models/NodeUptime";
+import {Node} from "../Models/Node";
+
+export class NodeUptimeService {
+
+    public async createNodeUptime(isWorking: boolean, nodeId: number) {
+        return await NodeUptime.create({
+            isWorking,
+            nodeId
+        });
+    }
+    public async getNodeUpTimeByData(nodeId: number) {
+        return await Node.findByPk(nodeId, {raw: true, include: [{model: NodeUptime}]});
+    }
+}
