b51a402a778240eaed5006679b305a485337021b
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-13 13:07:39 +0100

    Created service for registering users

---
+++ src/Services/UserService.ts
@@ -0,0 +1,18 @@
+import {User} from "../Models/User";
+
+export class UserService {
+    // eslint-disable-next-line
+    public async registerUser(email: string, hash_password: string) {
+        // eslint-disable-next-line
+        return await User.create({email, hash_password});
+    }
+
+    public async getUserByEmail(email: string) {
+        return await User.findOne({
+            raw: true,
+            where: {
+                email: email,
+            }
+        })
+    }
+}
