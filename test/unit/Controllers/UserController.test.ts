fae0d4d8e3b0671fabab1285e79c2ebcc16c704f
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-13 13:11:19 +0100

    Unit test for user registration

---
+++ test/unit/Controllers/UserController.test.ts
@@ -0,0 +1,38 @@
+import sinon from "sinon";
+import {expect} from "chai";
+import {Request, Response} from "express";
+import {UserService} from "../../../src/Services/UserService";
+import {UserController} from "../../../src/Controller/Api/UserController";
+import logger from "../../../src/Services/Logger";
+
+describe("UserController", function () {
+    describe('POST /user/register', () => {
+        const userServiceStub = sinon.createStubInstance(UserService);
+        // @ts-ignore
+        userServiceStub.registerUser.resolves({email: 'example@example.com', password: 'super secret password'});
+
+        it('should add new user to the database', async function () {
+            try {
+                const userController = new UserController(userServiceStub as unknown as UserService);
+                const response = {} as Response;
+                response.json = sinon.spy((result) =>
+                    expect(result.password).to.be.equal('super secret password')) as any;
+
+                response.status = sinon.spy((result) => {
+                    expect(result).to.equal(201)
+                    return response;
+                }) as any;
+
+                await userController.registerUser({
+                    body: {
+                        email: 'example@example.com',
+                        password: 'super secret password'
+                    }
+                } as Request, response)
+            } catch (err) {
+                logger.error('Unexpected error occured: ${err.message}');
+                expect.fail(err);
+            }
+        });
+    });
+});
