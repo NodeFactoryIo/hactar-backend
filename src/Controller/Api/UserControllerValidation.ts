9d9dc788f0fb5efc081e7822adf5214dfd56af3f
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-15 10:09:03 +0100

    Validation naming changes

--- src/Controller/Api/UserControllerValidation.ts
+++ src/Controller/Api/UserControllerValidation.ts
@@ -1,14 +1,14 @@
 import {ContainerTypes, ValidatedRequestSchema} from "express-joi-validation";
 import * as Joi from "@hapi/joi";
 
-export interface CreateUserRequestSchema extends ValidatedRequestSchema {
+export interface UserRequestSchema extends ValidatedRequestSchema {
     [ContainerTypes.Body]: {
         email: string;
         password: string;
     };
 }
 
-export const CreateUserValidationSchema = Joi.object({
+export const UserValidationSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
 });
