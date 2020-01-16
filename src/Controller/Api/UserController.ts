5f54ed3fa2a76d01589d40945364a396d3ee1545
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-16 13:00:25 +0100

    moved user authentication logic to the user service

--- src/Controller/Api/UserController.ts
+++ src/Controller/Api/UserController.ts
@@ -19,7 +19,7 @@ export class UserController {
     public async registerUser(req: ValidatedRequest<UserRequestSchema>, res: Response) {
         try {
             const {email, password} = req.body;
-            const user = await this.userService.getUserByEmail(email);
+            const user = await this.userService.isAuthenticatedUser(email, password);
             if (user) {
                 res.status(409).json({error: "There is already a user with this email address."})
             } else {
@@ -36,22 +36,16 @@ export class UserController {
     public async loginUser(req: ValidatedRequest<UserRequestSchema>, res: Response) {
         try {
             const {email, password} = req.body;
-            const user = await this.userService.getUserByEmail(email);
-            if (user) {
-                const isValidPassword = bcrypt.compareSync(password, user['hash_password']);
-                if (!isValidPassword) {
-                    res.status(401).json({error: "Unauthorized user."});
-                } else {
-                    const token = jwt.sign(user, config.jwtKey, {expiresIn: 86400});
-                    res.status(200).json({token: token})
-                }
+            const authenticatedUser = await this.userService.isAuthenticatedUser(email, password);
+            if (authenticatedUser) {
+                const token = jwt.sign({email}, config.jwtKey, {expiresIn: config.jwtExpiry});
+                res.status(200).json({token: token})
             } else {
-                res.status(404).json({error: "User not found."});
+                res.status(401).json({error: "Unauthorized user."});
             }
         } catch (e) {
             logger.error(`Error occurred on authorizing user in controller: ${e.message}`);
             res.status(500).json({error: "An uknown error occurred."});
-
         }
     }
 }
