91f46696a015e98b8214d129b625824a05b46ed2
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-21 11:18:13 +0100

    Moved logic to the user service

--- src/Controller/Api/UserController.ts
+++ src/Controller/Api/UserController.ts
@@ -1,12 +1,10 @@
 import {Response} from "express";
 import {ValidatedRequest} from "express-joi-validation";
-import * as bcrypt from "bcryptjs";
-import * as jwt from "jsonwebtoken";
 
 import logger from "../../Services/Logger";
-import config from "../../Config/Config";
 import {UserService} from "../../Services/UserService";
 import {UserRequestSchema} from "./UserControllerValidation";
+import {ServiceError} from "../../Services/ServiceError";
 
 export class UserController {
 
@@ -19,33 +17,35 @@ export class UserController {
     public async registerUser(req: ValidatedRequest<UserRequestSchema>, res: Response) {
         try {
             const {email, password} = req.body;
-            const user = await this.userService.isAuthenticatedUser(email, password);
-            if (user) {
-                res.status(409).json({error: "There is already a user with this email address."})
-            } else {
-                const hashPassword = bcrypt.hashSync(password, 10);
-                const result = await this.userService.registerUser(email, hashPassword);
+            const result = await this.userService.registerUser(email, password);
+            if (result) {
                 res.status(201).json(result)
             }
         } catch (e) {
-            logger.error(`Error occurred on creating user in controller: ${e.message}`);
-            res.status(500).json({error: "An uknown error occurred."});
+            if (e instanceof ServiceError) {
+                res.status(e.status).json({error: e.message});
+            } else {
+                logger.error(`Error occurred on registering user in controller: ${e.message}`);
+                res.status(500).json({error: "An uknown error occurred."});
+            }
         }
     }
 
     public async loginUser(req: ValidatedRequest<UserRequestSchema>, res: Response) {
         try {
             const {email, password} = req.body;
-            const authenticatedUser = await this.userService.isAuthenticatedUser(email, password);
-            if (authenticatedUser) {
-                const token = jwt.sign({email}, config.jwtKey, {expiresIn: config.jwtExpiry});
-                res.status(200).json({token: token})
-            } else {
-                res.status(401).json({error: "Unauthorized user."});
+            const result = await this.userService.authenticateUser(email, password);
+            if (result) {
+                res.status(200).json(result)
             }
         } catch (e) {
-            logger.error(`Error occurred on authorizing user in controller: ${e.message}`);
-            res.status(500).json({error: "An uknown error occurred."});
+            console.log(e)
+            if (e instanceof ServiceError) {
+                res.status(e.status).json({error: e.message});
+            } else {
+                logger.error(`Error occurred on authorizing user in controller: ${e.message}`);
+                res.status(500).json({error: "An uknown error occurred."});
+            }
         }
     }
 }
