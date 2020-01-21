64d38b67d20862e1e06cfc3a569e5c9f4c062c70
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-21 11:11:46 +0100

    Fixed user service register and authenticate user functions

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -1,29 +1,44 @@
 import * as bcrypt from "bcryptjs";
-import logger from "./Logger";
+import * as jwt from "jsonwebtoken";
+import config from "../Config/Config";
 
 import {User} from "../Models/User";
+import {ServiceError} from "./ServiceError";
 
-export class UserService {
+export class UserService extends ServiceError {
     // eslint-disable-next-line
-    public async registerUser(email: string, hash_password: string) {
+    public async registerUser(email: string, password: string) {
+        const user = await User.findOne({
+            raw: true,
+            where: {
+                email,
+            }
+        });
+
+        if (user) {
+            throw new ServiceError(409, "There is already a user with this email address.");
+        }
+        // eslint-disable-next-line
+        const hash_password = bcrypt.hashSync(password, 10);
         // eslint-disable-next-line
         return await User.create({email, hash_password});
     }
 
-    public async isAuthenticatedUser(email: string, password: string) {
-        try {
-            const user = await User.findOne({
-                raw: true,
-                where: {
-                    email,
-                }
-            })
-            if (user) {
-                return bcrypt.compareSync(password, user['hash_password']);
+    public async authenticateUser(email: string, password: string) {
+        const user = await User.findOne({
+            raw: true,
+            where: {
+                email,
             }
-            return false;
-        } catch (e) {
-            logger.error(`Error while searching for user: ${e.message}`);
+        });
+        if (user) {
+            const authenticatedUser = bcrypt.compareSync(password, user['hash_password']);
+            if (authenticatedUser) {
+                const token = jwt.sign({id: user['id']}, config.jwtKey, {expiresIn: config.jwtExpiry});
+                return token;
+            }
+            throw new ServiceError(401, "Unauthorized user.");
         }
+        throw new ServiceError(404, "User not found.");
     }
 }
