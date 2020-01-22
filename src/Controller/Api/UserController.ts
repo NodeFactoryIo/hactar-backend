7756550ba5e24a2f590a7bef49e8b724f7f2d891
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-22 19:22:21 +0100

    Moved function to nodes controller

--- src/Controller/Api/UserController.ts
+++ src/Controller/Api/UserController.ts
@@ -1,20 +1,17 @@
-import {Request, Response} from "express";
+import {Response} from "express";
 import {ValidatedRequest} from "express-joi-validation";
 
 import logger from "../../Services/Logger";
 import {UserService} from "../../Services/UserService";
 import {UserRequestSchema} from "./UserControllerValidation";
 import {ServiceError} from "../../Services/ServiceError";
-import {NodeService} from "../../Services/NodeService";
 
 export class UserController {
 
     private userService: UserService;
-    private nodeService: NodeService;
 
-    constructor(userService: UserService, nodeService: NodeService) {
+    constructor(userService: UserService) {
         this.userService = userService;
-        this.nodeService = nodeService;
     }
 
     public async registerUser(req: ValidatedRequest<UserRequestSchema>, res: Response) {
@@ -46,23 +43,6 @@ export class UserController {
                 res.status(e.status).json({error: e.message});
             } else {
                 logger.error(`Error occurred on authorizing user in controller: ${e.message}`);
-                res.status(500).json({error: "An uknown error occurred."});
-            }
-        }
-    }
-
-    public async getAllUserNodes(req: Request, res: Response) {
-        try {
-            const userId = res.locals.userId;
-            const result = await this.nodeService.getAllNodes(userId);
-            if (result) {
-                res.status(200).json(result)
-            }
-        } catch (e) {
-            if (e instanceof ServiceError) {
-                res.status(e.status).json({error: e.message});
-            } else {
-                logger.error(`Error occurred on fetching user nodes in controller: ${e.message}`);
                 res.status(500).json({error: "An uknown error occurred."});
             }
         }
