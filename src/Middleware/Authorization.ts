0dd85f2103867a123ee61809f87e6e89f0a6b22e
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-22 19:21:32 +0100

    Addressed PR comment --> renamed middleware and turning down request if unauthorized user

--- src/Middleware/Authorization.ts
+++ src/Middleware/Authorization.ts
@@ -1,23 +1,24 @@
 import express from "express";
 
-import {ServiceError} from "../Services/ServiceError";
-// import {JwtPayload} from "../Types/JwtPayloadType";
 import config from "../Config/Config";
 import * as jwt from "jsonwebtoken";
 import {JwtPayload} from "../Types/JwtPayloadType";
+import logger from "../Services/Logger";
 
-export async function Authorize(
+export async function AuthorizeUser(
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
 ) {
-    const token = (req.headers.authorization || req.headers.Authorization) as string;
-    const authorizedUser = await jwt.verify(token, config.jwtKey) as JwtPayload;
-
-    if (authorizedUser) {
-        res.locals.userId = authorizedUser.id;
-        next();
-    } else {
-        throw new ServiceError(403, "Unauthorized user.")
+    try {
+        const token = (req.headers.authorization || req.headers.Authorization) as string;
+        const authorizedUser = await jwt.verify(token, config.jwtKey) as JwtPayload;
+        if (authorizedUser) {
+            res.locals.userId = authorizedUser.id;
+            next();
+        }
+        res.status(403)
+    } catch (e) {
+        logger.error(`${e.name}: ${e.message}`)
     }
 }
