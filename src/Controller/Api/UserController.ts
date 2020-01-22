9e31e1576f2a70e50b6ff1059114f34e7f52001b
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-22 10:07:17 +0100

    Added function for fetching all user nodes

--- src/Controller/Api/UserController.ts
+++ src/Controller/Api/UserController.ts
@@ -1,17 +1,20 @@
-import {Response} from "express";
+import {Request, Response} from "express";
 import {ValidatedRequest} from "express-joi-validation";
 
 import logger from "../../Services/Logger";
 import {UserService} from "../../Services/UserService";
 import {UserRequestSchema} from "./UserControllerValidation";
 import {ServiceError} from "../../Services/ServiceError";
+import {AuthService} from "../../Services/AuthService";
 
 export class UserController {
 
     private userService: UserService;
+    private authService: AuthService;
 
-    constructor(userService: UserService) {
+    constructor(userService: UserService, authService: AuthService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
     public async registerUser(req: ValidatedRequest<UserRequestSchema>, res: Response) {
@@ -43,6 +46,23 @@ export class UserController {
                 res.status(e.status).json({error: e.message});
             } else {
                 logger.error(`Error occurred on authorizing user in controller: ${e.message}`);
+                res.status(500).json({error: "An uknown error occurred."});
+            }
+        }
+    }
+
+    public async getAllUserNodes(req: Request, res: Response) {
+        try {
+            const token = req.headers.authorization;
+            if (token) {
+                const userNodes = await this.authService.authorizerUser(token);
+                res.status(200).json(userNodes)
+            }
+        } catch (e) {
+            if (e instanceof ServiceError) {
+                res.status(e.status).json({error: e.message});
+            } else {
+                logger.error(`Error occurred on fetching user nodes in controller: ${e.message}`);
                 res.status(500).json({error: "An uknown error occurred."});
             }
         }
