4029f48e368fd051947847b3b5404664faaaf544
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-22 19:23:16 +0100

    Moved function from user contoller

--- src/Controller/Api/NodeController.ts
+++ src/Controller/Api/NodeController.ts
@@ -4,6 +4,7 @@ import {ValidatedRequest} from "express-joi-validation";
 import {NodeService} from "../../Services/NodeService";
 import logger from "../../Services/Logger";
 import {CreateNodeRequestSchema} from "./NodeControllerValidation";
+import {ServiceError} from "../../Services/ServiceError";
 
 export class NodeController {
 
@@ -37,6 +38,23 @@ export class NodeController {
         } catch (e) {
             logger.error(`Error occurred on deleting node in controller: ${e.message}`);
             res.status(500).json({error: "An unknown error occurred."});
+        }
+    }
+
+    public async getAllUserNodes(req: Request, res: Response) {
+        try {
+            const userId = res.locals.userId;
+            const result = await this.nodeService.getAllNodes(userId);
+            if (result) {
+                res.status(200).json(result)
+            }
+        } catch (e) {
+            if (e instanceof ServiceError) {
+                res.status(e.status).json({error: e.message});
+            } else {
+                logger.error(`Error occurred on fetching user nodes in controller: ${e.message}`);
+                res.status(500).json({error: "An uknown error occurred."});
+            }
         }
     }
 }
