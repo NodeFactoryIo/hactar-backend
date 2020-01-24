334e3ad773b45787e493d649a166a35d851c17f2
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-24 16:10:19 +0100

    Unit test for fetching disk information for a certain node

--- test/unit/Controllers/NodeDiskInformationController.test.ts
+++ test/unit/Controllers/NodeDiskInformationController.test.ts
@@ -4,6 +4,7 @@ import {Request, Response} from "express";
 import {NodeDiskInformationService} from "../../../src/Services/NodeDiskInformationService";
 import {NodeDiskInformationController} from "../../../src/Controller/Api/NodeDiskInformationController";
 import logger from "../../../src/Services/Logger";
+import {NodeDiskInformation} from "../../../src/Models/NodeDiskInformation";
 
 describe("NodeDiskInformation Controller", function () {
     describe('POST /diskinfo', () => {
@@ -28,6 +29,39 @@ describe("NodeDiskInformation Controller", function () {
                         freeSpace: 150,
                         takenSpace: 50,
                         nodeId: 4
+                    }
+                } as Request, response)
+            } catch (err) {
+                logger.error('Unexpected error occured: ${err.message}');
+                expect.fail(err);
+            }
+        });
+    });
+
+    describe('GET /user/node/diskinformation/nodeId', () => {
+        const nodeDiskInfoStub = sinon.createStubInstance(NodeDiskInformationService);
+        nodeDiskInfoStub.fetchDiskInfo.resolves([
+            {
+                "freeSpace": "125",
+                "takenSpace": "50",
+                "nodeId": 100
+            } as unknown as NodeDiskInformation
+        ]);
+
+        it('should return an array of disk information for a certain node', async function () {
+            try {
+                const nodeDiskInfoController = new NodeDiskInformationController(
+                    nodeDiskInfoStub as unknown as NodeDiskInformationService);
+                const response = {} as Response;
+                response.json = sinon.spy((result) => expect(result).to.be.an("Array")) as any;
+                response.status = sinon.spy((result) => {
+                    expect(result).to.equal(200)
+                    return response;
+                }) as any;
+
+                await nodeDiskInfoController.fetchNodeDiskInfo({
+                    params: {
+                        nodeId: 100
                     }
                 } as Request, response)
             } catch (err) {
