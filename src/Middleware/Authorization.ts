e073be3d795063f1fa79d2b82d633770ccbc476e
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-29 17:44:25 +0100

    Added check if the node belongs to user

--- src/Middleware/Authorization.ts
+++ src/Middleware/Authorization.ts
@@ -4,6 +4,9 @@ import config from "../Config/Config";
 import * as jwt from "jsonwebtoken";
 import {JwtPayload} from "../Types/JwtPayloadType";
 import logger from "../Services/Logger";
+import {Node} from "../Models/Node";
+import {ServiceError} from "../Services/ServiceError";
+
 
 export async function AuthorizeUser(
     req: express.Request,
@@ -13,10 +16,23 @@ export async function AuthorizeUser(
     try {
         const token = (req.headers.authorization || req.headers.Authorization) as string;
         const authorizedUser = await jwt.verify(token, config.jwtKey) as JwtPayload;
-        if (authorizedUser) {
+        const node = await Node.findByPk(req.params.nodeId)
+        if (node) {
+            if (authorizedUser.id !== node['userId']) {
+                throw new ServiceError(403, 'Unauthorized user.')
+            }
+            res.locals.node = node;
+        } else {
+            if (res.locals.node) {
+                const checkNode = res.locals.node
+                if (authorizedUser.id !== checkNode['userId']) {
+                    throw new ServiceError(403, 'Unauthorized user.')
+                }
+                res.locals.node = checkNode;
+            }
             res.locals.userId = authorizedUser.id;
-            next();
         }
+        next();
     } catch (e) {
         logger.error(`${e.name}: ${e.message}`);
         res.status(403).json({error: 'Unauthorized user.'})
