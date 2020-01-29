19bc8613af03f124f0e7a55278431af98e36431b
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-29 17:52:50 +0100

    Unit tests for middleware - user jwt scenarions

--- test/unit/Middleware/Authorization.test.ts
+++ test/unit/Middleware/Authorization.test.ts
@@ -1,58 +1,133 @@
 import sinon from "sinon";
 import {expect} from "chai";
-import {createSandbox, SinonSandbox} from "sinon";
 import logger from "../../../src/Services/Logger";
 import * as jwt from "jsonwebtoken";
 import config from "../../../src/Config/Config";
-import {JwtPayload} from "../../../src/Types/JwtPayloadType";
+import {Request, Response} from "express";
+import {AuthorizeUser} from "../../../src/Middleware/Authorization";
+// import {NodeService} from "../../../src/Services/NodeService";
+// import {Node} from "../../../src/Models/Node";
 
 describe("Authorization middleware", function () {
 
-    let sandbox: SinonSandbox;
-    let authStub: any;
-
-    // eslint-disable-next-line
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTAwLCJpYXQiOjE1Nzk3MDAxMTUsImV4cCI6MTU3OTc4NjUxNX0.Hd2RnG1KYfd-7c2ozNlZvzY0V2vVROT6CLWwrhGvXs4';
-
-    beforeEach(function () {
-        sandbox = createSandbox();
-        authStub = sinon.stub(jwt, "verify");
-    });
-
-    afterEach(function () {
-        sandbox.restore();
-        logger.silent = false;
-        authStub.restore();
-    });
-
     it("should successfully authorize user", async () => {
-        authStub.returns({
-            id: 100,
-            iat: 1579788404,
-            exp: 1579874804
-        })
+        const token = jwt.sign({id: 200}, config.jwtKey);
+        // const nodeServiceStub = sinon.createStubInstance(NodeService);
+        // nodeServiceStub.getNodeByPk.resolves(null);
+
         try {
-            const authorizedUser = await jwt.verify(token, config.jwtKey) as JwtPayload;
-            expect(authorizedUser).to.haveOwnProperty('id');
+
+            const response = {} as Response;
+            response.locals = {
+                node: {
+                    id: 400,
+                    url: "url11",
+                    token: "token111",
+                    address: "address111",
+                    userId: 200
+                }
+            }
+            // response.locals = {userId: 200}
+            response.json = sinon.spy((result) => {
+                if (result) {
+                    expect(result).to.be.equal(200)
+                    return response;
+                }
+            }) as any;
+
+            response.status = sinon.spy((result) => {
+                if (result) {
+                    expect(result).to.equal(200)
+                    return response;
+                }
+            }) as any;
+            const next = sinon.stub();
+
+            await AuthorizeUser({
+                headers: {
+                    // eslint-disable-next-line
+                    authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjAwLCJpYXQiOjE1ODAzMTIxNjUsImV4cCI6MTU4MDM5ODU2NX0.eWd_tH3pNaNXDQw8UPsQ_VjP-x4-JOm7vTCCqFRy9HQ`
+                },
+                body: {
+                    url: 'url111',
+                    address: 'address111'
+                }
+            } as Request, response, next);
+        } catch (err) {
+            console.log(err)
+            logger.error('Unexpected error occured: ${err.message}');
+            expect.fail(err);
+        }
+    });
+
+    it("should fail to authorize user with expired token", async () => {
+        const token = jwt.sign({id: 200}, config.jwtKey, {expiresIn: '0s'})
+
+        try {
+            const response = {} as Response;
+            response.json = sinon.spy((result) => {
+                if (result) {
+                    expect(result).to.have.property('error')
+                    return response;
+                }
+            }) as any;
+
+            response.status = sinon.spy((result) => {
+                if (result) {
+                    expect(result).to.equal(403)
+                    return response;
+                }
+            }) as any;
+
+            const next = sinon.stub();
+            await AuthorizeUser({
+                headers: {
+                    authorization: token
+                },
+                body: {
+                    url: 'url111',
+                    address: 'address111'
+                }
+            } as Request, response, next);
         } catch (err) {
             logger.error('Unexpected error occured: ${err.message}');
             expect.fail(err);
         }
     });
 
-    it("should fail to authorize an user without token", async () => {
+    it("should fail to authorize user which is not owner of the node", async () => {
         try {
-            await jwt.verify('', 'wrong secret') as JwtPayload;
-        } catch (err) {
-            expect(err.message).to.be.equal('jwt must be provided');
-        }
-    });
+            const token = jwt.sign({id: 300}, config.jwtKey, {expiresIn: '24h'})
 
-    it("should fail to authorize an user without valid token", async () => {
-        try {
-            await jwt.verify('123token456', config.jwtKey) as JwtPayload;
+            const response = {} as Response;
+            response.json = sinon.spy((result) => {
+                if (result) {
+                    expect(result).to.have.property('error')
+                    return response;
+                }
+            }) as any;
+
+            response.status = sinon.spy((result) => {
+                if (result) {
+                    expect(result).to.equal(403)
+                    return response;
+                }
+            }) as any;
+
+            const next = sinon.stub();
+            await AuthorizeUser({
+                headers: {
+                    authorization: token
+                },
+                body: {
+                    url: 'url111',
+                    address: 'address111',
+                    userId: 200
+                }
+            } as Request, response, next);
         } catch (err) {
-            expect(err.message).to.be.equal('jwt malformed');
+            logger.error('Unexpected error occured: ${err.message}');
+            expect.fail(err);
         }
     });
 });
