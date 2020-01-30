14baea635ea1f5babfc8fdee767bb8062ce3bd3f
Nikola Mlinaric <nikola@nodefactory.io>
2020-01-30 14:47:12 +0100

    Moved part of logic to passing node data middleware

--- src/Middleware/Authorization.ts
+++ src/Middleware/Authorization.ts
@@ -1,10 +1,8 @@
 import express from "express";
-
 import config from "../Config/Config";
 import * as jwt from "jsonwebtoken";
 import {JwtPayload} from "../Types/JwtPayloadType";
 import logger from "../Services/Logger";
-import {Node} from "../Models/Node";
 import {ServiceError} from "../Services/ServiceError";
 
 
@@ -16,22 +14,12 @@ export async function AuthorizeUser(
     try {
         const token = (req.headers.authorization || req.headers.Authorization) as string;
         const authorizedUser = await jwt.verify(token, config.jwtKey) as JwtPayload;
-        const node = await Node.findByPk(req.params.nodeId)
-        if (node) {
-            if (authorizedUser.id !== node['userId']) {
+        if (res.locals.node) {
+            if (authorizedUser.id !== res.locals.node['userId']) {
                 throw new ServiceError(403, 'Unauthorized user.')
             }
-            res.locals.node = node;
-        } else {
-            if (res.locals.node) {
-                const checkNode = res.locals.node
-                if (authorizedUser.id !== checkNode['userId']) {
-                    throw new ServiceError(403, 'Unauthorized user.')
-                }
-                res.locals.node = checkNode;
-            }
-            res.locals.userId = authorizedUser.id;
         }
+        res.locals.userId = authorizedUser.id;
         next();
     } catch (e) {
         logger.error(`${e.name}: ${e.message}`);
