a55fdc7ea02c56ddd59a2a1fccc2822bc400b317
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-03 13:00:19 +0100

    Created node general info migration

---
+++ src/Migrations/20200129194920-create-node-general-info.js
@@ -0,0 +1,46 @@
+var sequelize = require("sequelize");
+
+module.exports = {
+    up: function (queryInterface) {
+        return queryInterface.createTable('NodeGeneralInfo', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: sequelize.INTEGER
+            },
+            version: {
+                type: sequelize.STRING
+            },
+            sectorSize: {
+                type: sequelize.BIGINT
+            },
+            minerPower: {
+                type: sequelize.BIGINT
+            },
+            totalPower: {
+                type: sequelize.BIGINT
+            },
+            nodeId: {
+                type: sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Nodes',
+                    key: 'id'
+                },
+                unique: true
+            },
+            createdAt: {
+                allowNull: false,
+                type: sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: sequelize.DATE
+            }
+        })
+    },
+    down: function (queryInterface) {
+        return queryInterface.dropTable('NodeGeneralInfo');
+    }
+};
