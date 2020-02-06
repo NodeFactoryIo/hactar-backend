010e748a6bc4fd906ef44d4fe06c1be3be8682b4
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-06 11:42:02 +0100

    Created mining reward model

---
+++ src/Models/MiningReward.ts
@@ -0,0 +1,28 @@
+import {DataTypes, InitOptions, Model, ModelAttributes, Sequelize} from "sequelize";
+import {Node} from "./Node";
+
+export class MiningReward extends Model {
+
+    private cid: string;
+    private rewardAmount: string;
+    private nodeId: number;
+
+    public static initialize(sequelize: Sequelize) {
+        this.init({
+            cid: {
+                type: DataTypes.STRING(),
+                allowNull: false,
+            },
+            rewardAmount: {
+                type: DataTypes.STRING(),
+                allowNull: false,
+            },
+        } as ModelAttributes,
+            {
+                sequelize: sequelize,
+                tableName: "MiningRewards",
+            } as InitOptions);
+        this.belongsTo(Node, {foreignKey: "nodeId"});
+        Node.hasMany(MiningReward, {foreignKey: "nodeId"});
+    }
+}
