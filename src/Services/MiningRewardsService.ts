2835a7251f6301cad4e456fd2c87764055320e0f
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-06 14:53:25 +0100

    Addressed PR comment

--- src/Services/MiningRewardsService.ts
+++ src/Services/MiningRewardsService.ts
@@ -1,21 +1,25 @@
 import {MiningReward} from "../Models/MiningReward";
 import config from "../Config/Config";
 import {Node} from "../Models/Node";
-import {MiningRewardInput} from "../Types/MiningRewardType";
+import {MiningRewardInput} from "../Types/MiningRewardInputType";
+import logger from "./Logger";
 
 export class MiningRewardsService {
 
     public async storeMiningRewards(nodes: Array<Node>, rewards: Array<MiningRewardInput>) {
-        return await Promise.all(nodes.map(
-            (node: Node, index) => (
-                {
+        nodes.map(
+            async (node: Node, index) => {
+                const nodeToSave = {
                     cid: rewards[index].cid,
                     rewardAmount: config.rewardAmount,
                     nodeId: node.id,
+                };
+                try {
+                    return await MiningReward.create(nodeToSave)
+                } catch (e) {
+                    logger.error(`Error occurred on storing mining rewards: ${e}`);
                 }
-            )
-        ).map(async (node) => {
-            return await MiningReward.create(node);
-        }))
+            }
+        )
     }
 }
