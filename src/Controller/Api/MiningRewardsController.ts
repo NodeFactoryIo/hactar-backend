c20bfa59123dee38edcdbcd288f37158dd5af0d7
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-06 11:52:40 +0100

    Impoved logic for storing miner rewards

--- src/Controller/Api/MiningRewardsController.ts
+++ src/Controller/Api/MiningRewardsController.ts
@@ -4,6 +4,7 @@ import {ValidatedRequest} from "express-joi-validation";
 import logger from "../../Services/Logger";
 import {MiningRewardsService} from "../../Services/MiningRewardsService";
 import {CreateMiningRewardsRequestSchema} from "./MiningRewardsControllerValidation";
+import {MiningRewardInput} from "../../Types/MiningRewardType";
 
 export class MiningRewardsController {
 
@@ -15,8 +16,9 @@ export class MiningRewardsController {
 
     public async storeMiningRewards(req: ValidatedRequest<CreateMiningRewardsRequestSchema>, res: Response) {
         try {
-            const result = await this.miningRewardsService.storeMiningRewards(res.locals.miningRewards);
-            res.status(201).json({"Inserted records": result[1]});
+            const rewards = req.body as unknown as Array<MiningRewardInput>;
+            const result = await this.miningRewardsService.storeMiningRewards(res.locals.nodes, rewards);
+            res.status(201).json(result);
         } catch (e) {
             console.log('error', e)
             logger.error(`Error occurred on storing mining rewards in controller: ${e}`);
