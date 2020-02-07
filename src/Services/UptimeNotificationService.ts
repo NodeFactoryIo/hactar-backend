9eb495d7b1472501e4829c1195b30039975a4cc3
mak <mak@nodefactory.io>
2020-02-07 14:07:08 +0100

    Add UptimeNotificationService and add scheduler lib

--- src/Services/UptimeNotificationService.ts
+++ src/Services/UptimeNotificationService.ts
@@ -1,8 +1,36 @@
 import {UptimeNotification} from "../Models/UptimeNotification";
+import {NodeUptime} from "../Models/NodeUptime";
+import {EmailService} from "./EmailService";
+import {Node} from "../Models/Node";
+import {User} from "../Models/User";
 
 
 export class UptimeNotificationService {
-    public async createUptimeNotifications(nodeUptimeId: number) {
+
+    private emailService: EmailService;
+
+    constructor() {
+        this.emailService = new EmailService();
+    }
+
+    public async sendUptimeNotification(uptime: NodeUptime) {
+        const node = await Node.findByPk(uptime.nodeId);
+        if (node != null) {
+            const user = await User.findByPk(node.userId);
+            if (user != null) {
+                const emailSent = await this.emailService.sendEmailNotification(
+                    user,
+                    {NODE: node.address + node.url}
+                );
+                if (emailSent) {
+                    await this.createUptimeNotifications(uptime.id);
+                }
+            }
+        }
+    }
+
+
+    private async createUptimeNotifications(nodeUptimeId: number): Promise<UptimeNotification> {
         return UptimeNotification.create({
             nodeUptimeId
         });
