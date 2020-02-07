a305768a6a107cc062c8edf8379f2bc1f622840e
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-07 09:17:16 +0100

    Created migration file and node balance model

---
+++ src/Models/NodeBalance.ts
@@ -0,0 +1,22 @@
+import {DataTypes, InitOptions, Model, ModelAttributes, Sequelize} from "sequelize";
+import {Node} from "../Models/Node";
+
+export class NodeBalance extends Model {
+
+    private balance: string;
+
+    public static initialize(sequelize: Sequelize) {
+        this.init({
+            balance: {
+                type: DataTypes.STRING(),
+                allowNull: false,
+            },
+        } as ModelAttributes,
+            {
+                sequelize: sequelize,
+                tableName: "NodeBalance",
+            } as InitOptions);
+        this.belongsTo(Node, {foreignKey: "nodeId"});
+        Node.hasMany(NodeBalance, {foreignKey: "nodeId"});
+    }
+}
