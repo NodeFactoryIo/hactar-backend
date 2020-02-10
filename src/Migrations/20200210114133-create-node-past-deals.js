453f4968281edd58265d7d8db1181966be6d8837
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-10 15:49:55 +0100

    Created node past deal migration file and model

---
+++ src/Migrations/20200210114133-create-node-past-deals.js
@@ -0,0 +1,52 @@
+var sequelize = require("sequelize");
+
+module.exports = {
+    up: function (queryInterface) {
+        return queryInterface.createTable('NodePastDeals', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: sequelize.INTEGER
+            },
+            cid: {
+                type: sequelize.STRING
+            },
+            state: {
+                type: sequelize.INTEGER
+            },
+            size: {
+                type: sequelize.STRING
+            },
+            provider: {
+                type: sequelize.STRING
+            },
+            price: {
+                type: sequelize.STRING
+            },
+            duration: {
+                type: sequelize.INTEGER
+            },
+            nodeId: {
+                type: sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Nodes',
+                    key: 'id'
+                },
+                unique: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: sequelize.DataTypes.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: sequelize.DataTypes.DATE
+            }
+        });
+    },
+    down: function (queryInterface) {
+        return queryInterface.dropTable('NodePastDeals');
+    }
+};
---
+++ src/Models/NodePastDeal.ts
@@ -0,0 +1,49 @@
+import {DataTypes, InitOptions, Model, ModelAttributes, Sequelize} from "sequelize";
+import {Node} from "../Models/Node";
+
+export class NodePastDeal extends Model {
+
+    private cid: string;
+    private state: number;
+    private size: string;
+    private provider: string;
+    private price: string;
+    private duration: string;
+    private nodeId: number;
+
+    public static initialize(sequelize: Sequelize) {
+        this.init({
+            cid: {
+                type: DataTypes.STRING(),
+                allowNull: false,
+            },
+            state: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            size: {
+                type: DataTypes.STRING(),
+                allowNull: false,
+            },
+            provider: {
+                type: DataTypes.STRING(),
+                allowNull: false,
+            },
+            price: {
+                type: DataTypes.STRING(),
+                allowNull: false,
+            },
+            duration: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+        } as ModelAttributes,
+            {
+                sequelize: sequelize,
+                tableName: "NodePastDeals",
+                freezeTableName: true,
+            } as InitOptions);
+        this.belongsTo(Node, {foreignKey: "nodeId"});
+        Node.hasOne(NodePastDeal, {foreignKey: "nodeId"});
+    }
+}
