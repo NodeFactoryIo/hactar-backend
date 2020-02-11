3a2b2722565e3e5468e7e59d7881d599ea08cf25
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-11 09:11:02 +0100

    Addressed PR comment - changed space amounts and timestamps

--- src/Seeders/20200207084015-seed-NodeDiskInformation.js
+++ src/Seeders/20200207084015-seed-NodeDiskInformation.js
@@ -1,32 +1,32 @@
 module.exports = {
     up: (queryInterface) => {
         return queryInterface.bulkInsert('NodeDiskInformation', [{
-            freeSpace: 1203214536,
-            takenSpace: 453463,
+            freeSpace: 60979790489,
+            takenSpace: 13461441722,
             nodeId: 1,
-            createdAt: new Date().toLocaleString(),
-            updatedAt: new Date().toLocaleString()
+            createdAt: new Date('2019-08-06T14:05:49').toLocaleString(),
+            updatedAt: new Date('2019-08-06T14:05:49').toLocaleString()
         },
         {
-            freeSpace: 43980345823,
-            takenSpace: 32509235,
+            freeSpace: 252125334364,
+            takenSpace: 234464127878,
             nodeId: 1,
-            createdAt: new Date().toLocaleString(),
-            updatedAt: new Date().toLocaleString()
+            createdAt: new Date('2019-07-10T13:35:56').toLocaleString(),
+            updatedAt: new Date('2019-07-10T13:35:56').toLocaleString()
         },
         {
-            freeSpace: 684562576,
-            takenSpace: 536527,
+            freeSpace: 4338609729367,
+            takenSpace: 254324525262,
             nodeId: 2,
-            createdAt: new Date().toLocaleString(),
-            updatedAt: new Date().toLocaleString()
+            createdAt: new Date('2018-03-26T16:37:55').toLocaleString(),
+            updatedAt: new Date('2018-03-26T16:37:55').toLocaleString()
         },
         {
-            freeSpace: 3278470912,
-            takenSpace: 6275754,
+            freeSpace: 5184150281876,
+            takenSpace: 954334217064,
             nodeId: 2,
-            createdAt: new Date().toLocaleString(),
-            updatedAt: new Date().toLocaleString()
+            createdAt: new Date('2019-07-26T19:51:46').toLocaleString(),
+            updatedAt: new Date('2019-07-26T19:51:46').toLocaleString()
         }], {});
     },
 
