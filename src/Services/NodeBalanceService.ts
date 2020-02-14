213d5dcd01837a9004df50732a1ee60be67bbc26
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-14 14:04:23 +0100

    Addressed PR comment - refactored method for fetching node balance

--- src/Services/NodeBalanceService.ts
+++ src/Services/NodeBalanceService.ts
@@ -1,7 +1,7 @@
 import BigNumber from 'bignumber.js'
+import * as moment from "moment"
+import {Op} from "sequelize";
 import {NodeBalance} from "../Models/NodeBalance";
-import sequelize from "../Services/Database";
-import {QueryTypes} from "sequelize";
 import {ServiceError} from "./ServiceError";
 
 export class NodeBalanceService {
@@ -11,17 +11,17 @@ export class NodeBalanceService {
     }
 
     public async fetchNodeBalance(nodeId: number) {
-        const balances = await sequelize.sequelize.query(
-            `SELECT *
-                FROM "NodeBalance"
-                WHERE "nodeId" = 100
-                    AND "updatedAt" >= now() - interval '24 hours'
-                ORDER BY "updatedAt" ASC`,
-            {
-                replacements: {nodeId: nodeId},
-                type: QueryTypes.SELECT
-            }
-        ) as Array<NodeBalance>;
+        const balances = await NodeBalance.findAll({
+            raw: true,
+            where: {
+                nodeId,
+                updatedAt: {
+                    [Op.gte]:
+                        moment.utc().subtract(24, "hours").format("YYYY-MM-DD HH:MM:ssZZ")
+                }
+            },
+            order: [['updatedAt', 'ASC']]
+        });
         if (balances.length > 0) {
             const earliestRecord = new BigNumber(balances[0]['balance']);
             const latestRecord = new BigNumber(balances[balances.length - 1]['balance']);
