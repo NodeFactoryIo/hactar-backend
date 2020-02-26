8459e2b156c7b5bc89bacdb25d356201460c2864
Nikola Mlinaric <nikola@nodefactory.io>
2020-02-26 15:51:48 +0100

    Added method for updating user email and password

--- src/Controller/Api/UserController.ts
+++ src/Controller/Api/UserController.ts
@@ -1,4 +1,4 @@
-import {Response} from "express";
+import {Request, Response} from "express";
 import {ValidatedRequest} from "express-joi-validation";
 
 import logger from "../../Services/Logger";
@@ -62,6 +62,20 @@ export class UserController {
                 logger.error(`Error occurred on authorizing user daemon app in controller: ${e.message}`);
                 res.status(500).json({error: "An unknown error occurred."});
             }
+        }
+    }
+
+    public async updateUserAccount(req: Request, res: Response) {
+        try {
+            const {email, password} = req.body;
+            const userId = res.locals.userId;
+            const result = await this.userService.updateAccount(userId, email, password);
+            if (result) {
+                res.status(200).json(result)
+            }
+        } catch (e) {
+            logger.error(`Error occurred on updating user email/password in controller: ${e.message}`);
+            res.status(500).json({error: "An unknown error occurred."});
         }
     }
 }
