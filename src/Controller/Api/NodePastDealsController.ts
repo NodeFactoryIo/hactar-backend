eb1a5c3121a2a175285f3a47fd47ff26e9d6408c
Nikola Mlinaric <nikola@nodefactory.io>
2020-03-02 12:54:31 +0100

    Added method for fetching past deals

--- src/Controller/Api/NodePastDealsController.ts
+++ src/Controller/Api/NodePastDealsController.ts
@@ -2,7 +2,7 @@ import {Response} from "express";
 import {ValidatedRequest} from "express-joi-validation";
 
 import {NodePastDealsService} from "../../Services/NodePastDealsService";
-import {CreateNodePastDealsRequestSchema} from "./NodePastDealsControllerValidation";
+import {CreateNodePastDealsRequestSchema, FetchNodePastDealsRequestSchema} from "./NodePastDealsControllerValidation";
 import logger from "../../Services/Logger";
 import {NodePastDeal} from "../../Models/NodePastDealModel";
 
@@ -25,6 +25,22 @@ export class NodePastDealsController {
             res.status(200).json(result)
         } catch (e) {
             logger.error(`Error occurred on storing/updating node past deal in controller: ${e.message}`);
+            res.status(500).json({error: "An unknown error occurred."});
+        }
+    }
+
+    public async fetchNodePastDeals(req: ValidatedRequest<FetchNodePastDealsRequestSchema>, res: Response) {
+        try {
+            const userId = res.locals.userId;
+            const result = await this.nodePastDealsService.fetchNodePastDeals(
+                userId, req.query.from, req.query.to, req.query.orderBy);
+            if (result) {
+                res.status(200).json(result)
+            } else {
+                res.status(404).json({error: "No past deals found."});
+            }
+        } catch (e) {
+            logger.error(`Error occurred on fetching node past deals in controller: ${e.message}`);
             res.status(500).json({error: "An unknown error occurred."});
         }
     }
