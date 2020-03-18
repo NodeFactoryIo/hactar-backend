19b6a6ad7ac661afb5eacbeb4d915fac7248c140
Nikola Mlinaric <nikola@nodefactory.io>
2020-03-18 19:53:48 +0100

    Added node status by period in filter

--- src/Services/NodeUptimeService.ts
+++ src/Services/NodeUptimeService.ts
@@ -1,7 +1,5 @@
-import * as moment from "moment";
-import {unitOfTime} from "moment";
-import {Op} from "sequelize";
-
+import {QueryTypes} from "sequelize";
+import database from "../Services/Database";
 import {NodeUptime} from "../Models/NodeUptime";
 
 export class NodeUptimeService {
@@ -14,17 +12,26 @@ export class NodeUptimeService {
     }
 
     public async fetchNodeUptime(nodeId: number, filter: string) {
-        return await NodeUptime.findAll({
-            raw: true,
-            where: {
-                nodeId,
-                updatedAt: {
-                    [Op.gte]:
-                        moment.utc().subtract(1, filter as unitOfTime.Base).format("YYYY-MM-DD HH:MM:ssZZ")
-                }
-            },
-            order: [['updatedAt', 'DESC']]
-        });
+        return await database.runQuery<NodeUptime>(
+            `select "isWorking",date_trunc('day', "updatedAt") "timePeriod", (
+                case when "isWorking" = false
+                then 'Node is down'
+                else 'Node is up'
+                end) as "nodeStatus"
+                    from "NodeUptime"
+                    where "nodeId" = 1
+                    and "updatedAt" >= now() - interval '1 year'
+                    group by date_trunc('day', "updatedAt"), "isWorking"
+                    order by "timePeriod" desc;`,
+            {
+                replacements: {
+                    filter: `1 ${filter}`,
+                    nodeId: nodeId,
+                    period: filter == "day" ? "hour" : "day"
+                },
+                type: QueryTypes.SELECT
+            });
+
     }
 
     public async fetchLatestNodeUptime(nodeId: number) {
