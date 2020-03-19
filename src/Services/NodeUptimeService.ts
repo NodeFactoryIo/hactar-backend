99287bddbbf37f8057e2c9d8b153aa48b0d07c1f
Nikola Mlinaric <nikola@nodefactory.io>
2020-03-19 11:11:49 +0100

    Addressed PR comment - fixed logic and response layout

--- src/Services/NodeUptimeService.ts
+++ src/Services/NodeUptimeService.ts
@@ -1,6 +1,6 @@
-import {QueryTypes} from "sequelize";
 import database from "../Services/Database";
 import {NodeUptime} from "../Models/NodeUptime";
+import {filtersSelectQuery} from "../Utils/filterSelectQueryConfig";
 
 export class NodeUptimeService {
 
@@ -13,24 +13,17 @@ export class NodeUptimeService {
 
     public async fetchNodeUptime(nodeId: number, filter: string) {
         return await database.runQuery<NodeUptime>(
-            `select "isWorking",date_trunc('day', "updatedAt") "timePeriod", (
-                case when "isWorking" = false
-                then 'Node is down'
-                else 'Node is up'
-                end) as "nodeStatus"
-                    from "NodeUptime"
-                    where "nodeId" = 1
-                    and "updatedAt" >= now() - interval '1 year'
-                    group by date_trunc('day', "updatedAt"), "isWorking"
-                    order by "timePeriod" desc;`,
-            {
-                replacements: {
-                    filter: `1 ${filter}`,
-                    nodeId: nodeId,
-                    period: filter == "day" ? "hour" : "day"
-                },
-                type: QueryTypes.SELECT
-            });
+            `select *
+            from "NodeUptime"
+                where(date_trunc(:period, "updatedAt"), "updatedAt") in
+                (select date_trunc(:period, "updatedAt"), max("updatedAt")
+            from "NodeUptime"
+            where "nodeId" = :nodeId
+            group by date_trunc(:period, "updatedAt")
+                )
+            and "updatedAt" >= now() - interval :filter
+            order by "updatedAt" desc;`,
+            filtersSelectQuery(nodeId, filter));
 
     }
 
