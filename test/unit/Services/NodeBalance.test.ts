fedcf09eab37b62280f61e549bed65f9d2091e3d
Nikola Mlinaric <nikola@nodefactory.io>
2020-03-26 10:56:06 +0100

    Fixed old test when found balance and added new when on balance in last 24h

--- test/unit/Services/NodeBalance.test.ts
+++ test/unit/Services/NodeBalance.test.ts
@@ -1,5 +1,5 @@
 import {expect} from "chai";
-import {createSandbox, SinonSandbox} from "sinon";
+import {createSandbox, SinonSandbox, SinonStub} from "sinon";
 import {NodeBalanceService} from "../../../src/Services/NodeBalanceService";
 import logger from "../../../src/Services/Logger";
 import {NodeBalance} from "../../../src/Models/NodeBalance";
@@ -9,35 +9,30 @@ describe("NodeBalanceService", function () {
 
     let sandbox: SinonSandbox;
     let nodeBalanceService: NodeBalanceService;
-    let nodeBalanceFindStub: any;
+    let nodeBalanceFindAllStub: SinonStub<any>;
+    let nodeBalanceFindOneStub: SinonStub<any>;
 
     beforeEach(function () {
         sandbox = createSandbox();
         nodeBalanceService = new NodeBalanceService();
-        nodeBalanceFindStub = sinon.stub(NodeBalance, "findAll");
+        nodeBalanceFindAllStub = sinon.stub(NodeBalance, "findAll");
+        nodeBalanceFindOneStub = sinon.stub(NodeBalance, "findOne");
     });
 
     afterEach(function () {
         sandbox.restore();
         logger.silent = false;
-        nodeBalanceFindStub.restore();
+        nodeBalanceFindAllStub.restore();
+        nodeBalanceFindOneStub.restore();
     });
 
-    it("should return current node balance", async () => {
-        nodeBalanceFindStub.returns([
+    it("should return node balance with balance changes in the last 24 hours", async () => {
+        nodeBalanceFindAllStub.returns([
             {
-                "id": 3,
-                "balance": "8537124947501041",
-                "createdAt": "2020-01-13T06:41:48.000Z",
-                "updatedAt": "2020-02-17T06:41:48.000Z",
-                "nodeId": 1
-            },
-            {
-                "id": 2,
-                "balance": "78974914234696",
-                "createdAt": "2020-02-13T12:02:13.000Z",
-                "updatedAt": "2020-02-17T12:02:13.000Z",
-                "nodeId": 1
+                "currentBalance": "8537124947501041",
+                "updatedAt": "2020-03-26T06:41:48.000Z",
+                "balanceChange": "0",
+                "balanceChangePerc": "0.00%"
             }
         ]);
         try {
@@ -47,6 +42,31 @@ describe("NodeBalanceService", function () {
             expect(balances).to.haveOwnProperty('currentBalance');
             expect(balances).to.haveOwnProperty('updatedAt');
             expect(balances).to.haveOwnProperty('balanceChangePerc');
+
+        } catch (err) {
+            logger.error(`Unexpected error occured: ${err.message}`);
+            expect.fail(err);
+        }
+    });
+
+    it("should return latest node balance outside the last 24 hours", async () => {
+        nodeBalanceFindAllStub.returns([]);
+        nodeBalanceFindOneStub.returns(
+            {
+                "id": 2,
+                "balance": "8537124947501041",
+                "createdAt": "2020-01-13T06:41:48.000Z",
+                "updatedAt": "2020-02-17T06:41:48.000Z",
+                "nodeId": 5
+            } as unknown as NodeBalance);
+        try {
+            const balances = await nodeBalanceService.fetchNodeBalance(5);
+
+            expect(balances).to.exist;
+            expect(balances).to.haveOwnProperty('balance');
+            expect(balances).to.haveOwnProperty('createdAt');
+            expect(balances).to.haveOwnProperty('updatedAt');
+            expect(balances).to.haveOwnProperty('nodeId');
 
         } catch (err) {
             logger.error(`Unexpected error occured: ${err.message}`);
