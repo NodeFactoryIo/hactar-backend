ad6575389871edb86a9727603b66478522e814b5
nmlinaric <nikola@nodefactory.io>
2020-04-07 14:36:05 +0200

    Fix if es logging enabled

--- src/Services/Logger.ts
+++ src/Services/Logger.ts
@@ -31,7 +31,7 @@ const esTransportOpts = {
     } as ClientOptions,
 };
 
-if (config.env == "production" && config.logging.es.enabled) {
+if (config.env == "production" && config.logging.es === "enabled") {
     logger.add(new (ElasticsearchTransport as any)(esTransportOpts));
 }
 
